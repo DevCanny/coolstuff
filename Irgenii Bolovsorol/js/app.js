629849607f3d51925703f2cd7c9ff7749f69adaf
DevCanny <79893805+DevCanny@users.noreply.github.com>
2022-03-09 17:25:57 +0800

    Add files via upload

---
+++ Irgenii Bolovsorol/js/app.js
@@ -0,0 +1,37 @@
+var StuffToChange = document.getElementById("One_Title")
+var StuffToAlt = document.getElementById("One_alt")
+var Num = 0
+
+document.addEventListener("keydown", e => {
+    var WaitTime = 700
+    var ToShowSlide2 = [
+        "Эелдэг байх",
+        "please",
+        "fill me",
+        "for",
+        "damn",
+        "sake",
+    ]
+    if (e.keyCode == 48){
+        StuffToChange.innerHTML = "Бусадтай"
+        setTimeout(function(){
+            StuffToChange.innerHTML = "Бусадтай зөв"
+        },WaitTime)
+        setTimeout(function(){
+            StuffToChange.innerHTML = "Бусадтай зөв ойлголцохын"
+        },WaitTime * 2)
+        setTimeout(function(){
+            StuffToChange.innerHTML = "Бусадтай зөв ойлголцохын ач"
+        },WaitTime * 3)
+        setTimeout(function(){
+            StuffToChange.innerHTML = "Бусадтай зөв ойлголцохын ач тус"
+        },WaitTime * 4)
+        setTimeout(function(){
+            StuffToAlt.innerHTML = "haha stinky poopoo lol"
+        },WaitTime * 6)
+    } else if(e.keyCode == 57) {
+        if(Num == ToShowSlide2.length){return}
+        document.getElementById("Two_Point"+Num).innerHTML = "· " + ToShowSlide2[Num]
+        Num++
+    }
+})
